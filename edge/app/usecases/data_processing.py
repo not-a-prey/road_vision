7be9f216e3a754e2987924b5059836f625a8656e
Marat Skyba <136718996+fujivara@users.noreply.github.com>
2026-03-03 21:10:46 +0200

    Lab 4 (#4)
    * lab 4 implementation
    
    * upd

--- edge/app/usecases/data_processing.py
+++ edge/app/usecases/data_processing.py
@@ -2,6 +2,10 @@ from app.entities.agent_data import AgentData
 from app.entities.processed_agent_data import ProcessedAgentData
 
 
+Z_AXIS_POTHOLE_THRESHOLD = 16600
+Z_AXIS_BUMP_THRESHOLD = 16450
+
+
 def process_agent_data(
     agent_data: AgentData,
 ) -> ProcessedAgentData:
@@ -12,4 +16,11 @@ def process_agent_data(
     Returns:
         processed_data_batch (ProcessedAgentData): Processed data containing the classified state of the road surface and agent data.
     """
-    # Implement it
+    z_acceleration = agent_data.accelerometer.z
+    if z_acceleration > Z_AXIS_POTHOLE_THRESHOLD:
+        road_state = "pothole"
+    elif z_acceleration < Z_AXIS_BUMP_THRESHOLD:
+        road_state = "bump"
+    else:
+        road_state = "good"
+    return ProcessedAgentData(road_state=road_state, agent_data=agent_data)
